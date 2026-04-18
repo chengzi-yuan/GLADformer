20883861b354de83dc5cd509e3bb5ceb5c27ddb8
chengziyuan <cam0987268392@gmail.com>
2026-04-18 18:29:21 +0800

    Update train.py

--- train.py
+++ train.py
@@ -21,8 +21,8 @@ parser.add_argument('--no_autocast', action='store_false', default=True, help='d
 parser.add_argument('--save_dir', default='./saved_models/', type=str, help='path to models saving')
 parser.add_argument('--data_dir', default='./data/', type=str, help='path to dataset')
 parser.add_argument('--log_dir', default='./logs/', type=str, help='path to logs')
-parser.add_argument('--dataset', default='RESIDE-IN', type=str, help='dataset name')
-parser.add_argument('--exp', default='indoor', type=str, help='experiment setting')
+parser.add_argument('--dataset', default='RSHaze', type=str, help='dataset name')
+parser.add_argument('--exp', default='rshaze', type=str, help='experiment setting')
 parser.add_argument('--gpu', default='0,1,2,3', type=str, help='GPUs used for training')
 args = parser.parse_args()
 
@@ -35,7 +35,6 @@ def train(train_loader, network, criterion, optimizer, scaler, epoch):
     torch.cuda.empty_cache()
     network.train()
 
-    # [修改点] 使用 tqdm 包装 train_loader 以实时显示 Loss
     loop = tqdm(train_loader, desc=f'Train Epoch {epoch}', leave=True)
 
     for batch in loop:
