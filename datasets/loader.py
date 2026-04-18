5d6396d493ef7ef26fde2c717ef84030ba3c09d9
chengziyuan <cam0987268392@gmail.com>
2026-04-18 21:08:39 +0800

    Update loader.py

--- datasets/loader.py
+++ datasets/loader.py
@@ -11,7 +11,6 @@ def augment(imgs=[], size=256, edge_decay=0., only_h_flip=False):
 	H, W, _ = imgs[0].shape
 	Hc, Wc = [size, size]
 
-	# simple re-weight for the edge. chengzi
 	if random.random() < Hc / H * edge_decay:
 		Hs = 0 if random.randint(0, 1) == 0 else H - Hc
 	else:
@@ -98,7 +97,6 @@ class SingleLoader(Dataset):
 		cv2.setNumThreads(0)
 		cv2.ocl.setUseOpenCL(False)
 
-		# read image, and scale [0, 1] to [-1, 1]. chengzi
 		img_name = self.img_names[idx]
 		img = read_img(os.path.join(self.root_dir, img_name)) * 2 - 1
 
